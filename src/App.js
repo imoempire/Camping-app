5dd959a088f6177628496f13532565c7269a69d4
imoempire <mrimoempire@gmail.com>
2021-09-01 18:57:18 +0000

    import camplist.js

--- src/App.js
+++ src/App.js
@@ -1,36 +1,12 @@
-import React, { Component } from 'react';
-import Person from './Components/Person';
-import PersonsForm from './Components/PersonsForm';
+import React from 'react';
+import CamperList from './Components/CamperList';
 
-class App extends Component {
-  constructor(props){
-    super(props);
-    this.state = {
-      Persons : [
-        { Name: "John", Age: "20", Room: "20", CampColor: "red", id: "ug374398ir3"}
-      ]
-    }
-  };
-    AddHandle = (newPerson)=>{
-      newPerson.id = Math.random().toString()
-      this.setState({
-        Persons: [...this.state.Persons, newPerson]
-      })
-    }
-  render() {
-    const Persons = this.state.Persons.map((person, index)=>{
-      return(
-        <Person Persons={person} index={index}/>
-      )
-    })
-    return (
-      <div>
-        <PersonsForm addPerson={this.AddHandle}/>
-        <br />
-        {Persons}
-      </div>
-    );
-  }
+const App = () => {
+  return (
+    <div>
+      <CamperList />
+    </div>
+  );
 }
 
 export default App;
